2a1cff8a401e39b674c65e408ef0a6fd7017ca6c
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2022-01-23 19:07:30 +0530

    ard

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+})
+
+
+function jump(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  
